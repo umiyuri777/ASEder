43812cd40b0ea0b058f2c780d838b2337e2b908f
inoueshoei <inoueshoei.313.9720@gmail.com>
2024-10-20 11:37:52 +0900

    [Fix]Port変更修正

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,5 @@ RUN pip install --upgrade pip setuptools
 COPY requirements.txt /bot/
 RUN pip install -r requirements.txt
 
-EXPOSE 8080
-
 COPY . /bot
 CMD ["python3", "main.py"]
--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,8 @@ def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
